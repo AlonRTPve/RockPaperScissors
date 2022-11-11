6effdb8a9fa49dc954acb987a982584830cbdd09
AlonRTPve <alonshlomi@hotmail.com>
2022-11-11 17:23:31 +0200

    Finished the project

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 choices = ["rock", "paper", "scissors"]
 
 
+
+const choiceBtns = document.querySelectorAll(".choicebtn");
+
+const Playertext = document.querySelector("#playerresult");
+const ComputerText = document.querySelector("#computerresult");
+const scoretext = document.querySelector("#score");
+
+
+let score= 0;
+let computerscore = 0;
+
 function calculate_result(player_choice, computer_choice){
 
     if (computer_choice == player_choice) {
@@ -8,27 +19,30 @@ function calculate_result(player_choice, computer_choice){
     }
     
     else if (player_choice == "rock" && computer_choice == "scissors"){  //player wins
-        return "Won"
+        return ["You won rock beats scissors", "Won"]
     }
 
     else if (player_choice == "scissors" && computer_choice == "paper"){  //player wins
-        return "Won"
+        return ["You won scissors beat paper", "Won"]
     }
 
     else if (player_choice == "paper" && computer_choice == "rock"){  //player wins
-        return "Won"
+        return ["You won paper beats rock", "Won"]
     }
 
     else if (player_choice == "rock" && computer_choice == "paper"){  //player losses
-        return "lost"
+        return ["You lost paper beats rock", "Lost"]
     }
 
     else if (player_choice == "scissors" && computer_choice == "rock"){  //player losses
-        return "lost"
+        return ["You lost rock beat scissors", "Lost"]
     }
 
     else if (player_choice == "paper" && computer_choice == "scissors"){  //player losses
-        return "lost"
+        return ["You lost scissors beat paper", "Lost"]
+    }
+    else{
+        console.log("something_wrong")
     }
 }
 
@@ -41,8 +55,47 @@ function GetRandomChoice (){
 
 
 
-const btn=document.getElementById("rock");
 
-btn.addEventListener("click", function(){
-    alert("You clicked me");
- })
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computer= GetRandomChoice();
+
+    Playertext.textContent = `Player: ${player}`
+    ComputerText.textContent = `computer: ${computer}`
+
+    result = calculate_result(player, computer)
+    if (result == "Tie"){
+        scoretext.textContent = `Result: its a tie! player: ${score} computer: ${computerscore}`
+    }
+    else if (result[1] == "Won"){
+        score = score+1;
+        scoretext.textContent = `${result[0]}  SCORE: player: ${score} computer: ${computerscore}`
+
+    }
+
+    else if (result[1] == "Lost"){
+        computerscore = computerscore+1;
+        scoretext.textContent = `${result[0]}  SCORE: player: ${score} computer: ${computerscore}`
+            
+
+    }
+    if (computerscore == 5 ){
+        for (let i=0; i<choiceBtns.length; i++){
+            choiceBtns[i].disabled = true;
+
+        }
+        scoretext.textContent = `You lost! Refresh the page to play again!`
+        return
+    }
+    if (score == 5 ){
+        for (let i=0; i<choiceBtns.length; i++){
+            choiceBtns[i].disabled = true;
+
+        }
+        scoretext.textContent = `You won! Refresh the page to play again!`
+        return
+    }
+    
+    
+}))
+
